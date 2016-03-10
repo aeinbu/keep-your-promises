1ffb16bca3b0adb8f24c7cb6e03be63fd41c2bef
Arjan Einbu <arjan@einbu.no>
2016-03-10 23:41:25 +0100

    Initial

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var fs = require("fs");
+var _ = require("lodash")
+
+app.use(function(req, res, next) {
+    res.header("Content-Type", "application/json; charset=utf-8");
+    next();
+});
+
+app.get("/fylker", function (req, res) {
+    fs.readFile("./data.json", "utf-8", function(err, data){
+        data = JSON.parse(data);
+        data = _.keys(data);
+            
+        return res.send(data);
+    });
+});
+
+app.get("/kommuner/i/:id", function (req, res) {
+    fs.readFile("./data.json", "utf-8", function(err, data){
+        data = JSON.parse(data);
+        data = data[req.params.id];
+        data = data.map(item => item.kommune);
+        
+        return res.send(data);
+    });
+});
+
+
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("App listening at http://%s:%s", host, port);
+});
